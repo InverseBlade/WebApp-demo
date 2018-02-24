aaef6eabd969e31604aeb61df0ffef846b83fadc
zewei zhang <thicksky@126.com>
2018-02-24 10:24:26 +0800

    bug fix

--- public/static/Assets/js/index.js
+++ public/static/Assets/js/index.js
@@ -4,4 +4,36 @@
         //Token.message.alert("你好！")
     });
 
+    (function () {
+        var ifload = false;
+        var touchTime = new Date().getTime();
+        var render_tab2 = Template7.compile($$('script#tab-2-template').html());
+
+        function loadPageContent() {
+            if(ifload) return;
+            BookApp.preloader.show();
+            ifload = true;
+
+            window.setTimeout(function () {
+                $$('.page-content#tab-2').html(render_tab2());
+
+                BookApp.preloader.hide();
+                ifload = false;
+            }, 500);
+        }
+
+        $$('a[href="#tab-2"]').on('click', function (e) {
+            if(new Date().getTime() - touchTime < 250){
+                $$('.page-content#tab-2').html('');
+                loadPageContent();
+            }else{
+                touchTime = new Date().getTime();
+                if('' == $$('.page-content#tab-2').html().trim()){
+                    loadPageContent();
+                }
+            }
+        });
+
+    })();
+
 })(window);
