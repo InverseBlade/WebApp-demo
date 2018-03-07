d9f261c8305793dc3073d2658ee68001ccf6b62a
Catalan <thicksky@126.com>
2018-03-07 19:57:58 +0800

    添加留言发布界面

--- public/static/Assets/js/index.js
+++ public/static/Assets/js/index.js
@@ -1,6 +1,6 @@
 (function (window) {
 
-    (function () {
+    function render_mainPage() {
         target = $$('.view-main').find('.components-list');
         render = Template7.compile($$('script#tab-1-template').html());
         BookApp.request.post('/GuestBook/Essay/api',
@@ -29,7 +29,8 @@
                 }
                 target.append(html);
             });
-    })();
+    }
+    render_mainPage();
 
     $$('.view-main').find('a.tab-1').addClass('tab-link-active');
     $$('a.tab-link').on('click', function (e) {
@@ -54,4 +55,53 @@
         $$(this).find('textarea').focus();
     });
 
+    $$('a.publish-button').on('click', function (e) {
+        var target = $$('div.popup').find('textarea.essay-publish');
+        var content = target.val();
+
+        if(content.length == 0 || content == ''){
+            Token.message.alert("留言不能为空！");
+            return;
+        }
+        var topic = 'all test';
+        BookApp.request.post('/GuestBook/essay/add',
+            {
+                topic : topic,
+                content : content
+            },
+            function (result) {
+                result = JSON.parse(result);
+
+                if(result.err_code == 1){
+                    Token.message.alert("发送失败！");
+                    return;
+                }
+                $$('.view-main').find('.components-list').html('');
+                render_mainPage();
+                Token.message.alert("留言发布成功！", function () {
+                    target.val('');
+                    BookApp.popup.close('.popup');
+                });
+            });
+    });
+
 })(window);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
